2efd86a70efd3f491dd9b89cabab2d0f8b3d720b
ebrahimkhan360 <md.ebrahim.khan.w@gmail.com>
2022-05-22 16:35:26 +0600

    Footer add .

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Navbar from './Pages/Home/Header/Navbar/Navbar';
 import Home from './Pages/Home/Home/Home';
+import Footer from './Pages/Shared/Footer/Footer';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Routes>
         <Route path='/' element={<Home></Home>}></Route>
       </Routes>
+      <Footer></Footer>
     </div>
   );
 }
--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -1,10 +1,12 @@
 import React from 'react';
+
 import Banner from '../Banner/Benner';
 
 const Home = () => {
     return (
         <div>
            <Banner></Banner> 
+           
         </div>
     );
 };
